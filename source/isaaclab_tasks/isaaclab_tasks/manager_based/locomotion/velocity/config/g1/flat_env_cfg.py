13ec041e8dca20a8165fc57789c4ad348a3cfc4d
qxjfly <qxjfly@github.com>
2025-04-27 20:15:53 +0800

    20250427_witharm

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/flat_env_cfg.py
@@ -40,7 +40,7 @@ class G1FlatEnvCfg(G1RoughEnvCfg):
         # Commands
         self.commands.base_velocity.ranges.lin_vel_x = (0.0, 1.0) #default (0.0, 1.0)
         self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
-        self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
+        self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)# qxj
         
 
 class G1FlatEnvCfg_PLAY(G1FlatEnvCfg):
